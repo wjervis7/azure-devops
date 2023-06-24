595cf95a6c5110d0f69e7a3de59f313c2e7976d8
Warren Jervis <warren.jervis@pm.me>
2023-06-24 04:47:38 -0700

    test

--- src/utilities/__tests__/dateTIme.spec.ts
+++ src/utilities/__tests__/dateTIme.spec.ts
@@ -5,8 +5,8 @@ import { displayDate } from "../dateTime";
 describe("displayDate", () => {
     it("parses and formats ISO dates correctly", () => {
         // arrange
-        Settings.defaultLocale = "en-us";
-        const isoDate = "2016-05-25T09:24:15.123";
+                    Settings.defaultLocale = "en-us";
+        const isoDate = "2016-05-25T09:24:15.123";;
         const expected = "5/25/2016, 9:24 AM";
 
         // act
